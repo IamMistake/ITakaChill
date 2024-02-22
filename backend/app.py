e5b858c0e4d3017656f74d59aff8b197e733deaa
andreej21 <maxcarleone967@gmail.com>
2024-02-22 22:48:49 +0100

    fetch movies

--- backend/app.py
+++ backend/app.py
@@ -6,12 +6,20 @@ import json
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-@app.route('/')
-def getIndex():
-    return "Hello World!"
 
+
+@app.route("/movies")
+def get_movies():
+
+    res = {}
+    with open("ITakaChill\\backend\\static\\filmovi.json",'r') as filmovi:
+        filmovi_dict = json.load(filmovi)
+
+    
+    return filmovi_dict
 
 
 
 if __name__ == '__main__':
+    get_movies()
     app.run(debug=True)
