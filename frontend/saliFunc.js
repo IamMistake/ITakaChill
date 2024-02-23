cd1bbee0df80815c66c8954a611121cce0e27c1e
IamMistake <jagurinoskini@gmail.com>
2024-02-23 01:07:47 +0100

    Sali better

--- frontend/saliFunc.js
+++ frontend/saliFunc.js
@@ -1,7 +1,8 @@
 let id = 1;
 
 let banner = document.getElementById("banner");
-let sali = document.getElementById("sali")
+let sali = document.getElementById("sali");
+let chairsElem = document.getElementById("place")
 
 doStuff();
 
@@ -14,12 +15,38 @@ async function fetchData(pth) {
     }
 }
 
-function openSala(ova) {
+function chairs(x) {
+    let matrix = x.matrix[0];   // 0 E KOJA SALA CE BIDIT
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            if(matrix[i][j] === 1) {
+                let div = `<div id="${"" + i + j}" class="zafatenaChair"></div>`
+                chairsElem.innerHTML += div;
+            } else {
+                let div = `<div id="${"" + i + j}" class="nezafatenaChair"></div>`
+                chairsElem.innerHTML += div;
+            }
+        }
+        chairsElem.innerHTML += `<div></div>`;
+    }
+}
 
+
+function openSala(x) {
+    // const n = x.saliNumber;
+    const dates = x.dates;
+    const times = x.times;
+    for (let i = 1; i <= dates.length; i++) {
+        sali.innerHTML += `<div class="sala">
+        ${dates[i - 1]}
+        <p class="vreme" id="${"vreme" + i}">${times[i - 1]}</p>
+    </div>
+    <div class="borderSala"></div>`
+    }
 }
 
 function thumbnail(x) {
-    let bgUrl =
+    let bgUrl = x.thumbnail;
     banner.style.backgroundImage = `url(${bgUrl})`;
 }
 
@@ -28,11 +55,12 @@ async function doStuff() {
 
     const saliData = await fetchData("../backend/static/sali.json");
     saliData.forEach(x => {
-        if(x == id) {
+        // console.log(x)
+        if(x.id === id) {
+            console.log(x)
             openSala(x);
             thumbnail(x);
+            chairs(x)
         }
     })
-
-    // sali.innerHTML += ``
 }
